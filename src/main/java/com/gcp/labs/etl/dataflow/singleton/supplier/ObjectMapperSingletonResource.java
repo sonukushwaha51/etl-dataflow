66ed5a570f2f6bd299ef17c4ac339b3aac2f6c51
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-10-26 16:16:32 +0530

    added properties to pipeline options

--- src/main/java/com/gcp/labs/etl/dataflow/singleton/supplier/ObjectMapperSingletonResource.java
+++ src/main/java/com/gcp/labs/etl/dataflow/singleton/supplier/ObjectMapperSingletonResource.java
@@ -1,15 +1,14 @@
 package com.gcp.labs.etl.dataflow.singleton.supplier;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gcp.labs.etl.dataflow.singleton.SerializerSupplier;
 import com.gcp.labs.etl.dataflow.singleton.SingletonResource;
 import com.google.inject.Inject;
 
 public class ObjectMapperSingletonResource extends SingletonResource<ObjectMapper> {
 
     @Inject
-    public ObjectMapperSingletonResource(SerializerSupplier<ObjectMapper> serializerSupplier) {
-        super(serializerSupplier);
+    public ObjectMapperSingletonResource(ObjectMapperSupplier objectMapperSupplier) {
+        super(objectMapperSupplier);
     }
 
     @Override
